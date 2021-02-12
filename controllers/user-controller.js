a2e3a98507934635b3109f33314677dde4c99b8a
Mike Rivera <michaelstephenrivera@gmail.com>
2021-02-12 13:50:58 -0600

    Created route/controller for updating a single user & deleting a single user by id

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,16 @@
 const { User } = require('../models')
 
 const userController = {
+  // Create a new user
+  createUser: (req, res) => {
+    User.create(req.body)
+      .then(dbUserData => res.status(201).json(dbUserData))
+      .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+      })
+  },
+  // Get all users
   getUsers: (req, res) => {
     User.find()
       .select('-__v')
@@ -10,14 +20,51 @@ const userController = {
         res.status(500).json(err)
       })
   },
-  createUser: (req, res) => {
-    User.create(req.body)
-      .then(dbUserData => res.status(201).json(dbUserData))
+  // Get single user by id
+  getSingleUser: (req, res) => {
+    User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      .populate('friends')
+      .populate('thoughts')
+      .then(dbUserData => {
+        !dbUserData ? res.status(404).res.json({ message: `No user exists with id:${req.params.userId}` })
+          : res.json(dbUserData)
+      })
+      .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+      })
+  },
+  updateUser: (req, res) => {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      {
+        runValidators: true,
+        new: true
+      }
+    )
+      .then(dbUserData => {
+        !dbUserData ? res.status(404).json({ message: `No user exists with id:${req.params.userId}` })
+          : res.json(dbUserData)
+      })
+      .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+      })
+  },
+  deleteUser: (req, res) => {
+    User.findOneAndDelete({ _id: req.params.userId })
+      .then(dbUserData => {
+        !dbUserData ? res.status(404).json({ message: `No user exists with id:${req.params.userId}` })
+          : res.json(dbUserData)
+      })
       .catch(err => {
         console.log(err)
         res.status(500).json(err)
       })
   }
+
 }
 
 module.exports = userController
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,13 +1,22 @@
 const router = require('express').Router()
 const {
   createUser,
-  getUsers
+  getUsers,
+  getSingleUser,
+  updateUser,
+  deleteUser
 } = require('../../controllers/user-controller')
 
 // /api/users
 router.route('/')
   .post(createUser)
   .get(getUsers)
+
+// /api/users/:userId
+router.route('/:userId')
+  .get(getSingleUser)
+  .put(updateUser)
+  .delete(deleteUser)
 
 
 module.exports = router
